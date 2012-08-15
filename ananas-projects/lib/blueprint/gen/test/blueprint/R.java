1d57e33610f654f0415048ae54d2c540e1196d7e
xukun0217 <xukun0217@gmail.com>
2012-08-15 13:16:02 +0800

    ignore gen dir

---
+++ ananas-projects/lib/blueprint/gen/test/blueprint/R.java
@@ -0,0 +1,24 @@
+package test.blueprint;
+// This file is generate by {ananas.lib.blueprint.tools.ResourceIdGen$ResultSet@dfbb43} tool.
+// Don't modify it !
+class R {
+    public static class id {
+        public static final String __id_menu_new_window = "@+id/menu_new_window";
+        public static final String __id_desktop = "@+id/desktop";
+        public static final String id_menu_exit = "id_menu_exit";
+        public static final String root = "root";
+        public static final String id_button_5 = "id_button_5";
+        public static final String id_button_2 = "id_button_2";
+        public static final String id_button_hello = "id_button_hello";
+        public static final String id_button_1 = "id_button_1";
+        public static final String id_button_south = "id_button_south";
+        public static final String id_button_4 = "id_button_4";
+        public static final String id_button_3 = "id_button_3";
+    }
+    public static class file {
+        public static final String demo_xml = "resource:///demo.xml";
+        public static final String test_xml = "resource:///test.xml";
+        public static final String test_mdi_child_xml = "resource:///test-mdi-child.xml";
+        public static final String test_mdi_xml = "resource:///test-mdi.xml";
+    }
+}
