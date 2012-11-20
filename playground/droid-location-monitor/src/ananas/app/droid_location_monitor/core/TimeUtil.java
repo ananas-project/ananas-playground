0bebf44e8024e5a2a98bb91f1876d0d3bae4b1a5
xukun <xukun17@sina.com>
2012-11-20 13:21:09 +0800

    xxx

--- playground/droid-location-monitor/src/ananas/app/droid_location_monitor/core/TimeUtil.java
+++ playground/droid-location-monitor/src/ananas/app/droid_location_monitor/core/TimeUtil.java
@@ -1,5 +1,8 @@
 package ananas.app.droid_location_monitor.core;
 
+import java.util.Calendar;
+import java.util.TimeZone;
+
 public class TimeUtil {
 
 	public static String timespanToString(long ms) {
@@ -17,8 +20,37 @@ public class TimeUtil {
 	}
 
 	public static String timestampToString(long ms) {
-		// TODO Auto-generated method stub
-		return ms + "";
+
+		Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("GMT"));
+		cal.setTimeInMillis(ms);
+
+		int year = cal.get(Calendar.YEAR);
+		int month = cal.get(Calendar.MONTH) + 1;
+		int day = cal.get(Calendar.DAY_OF_MONTH);
+		int h = cal.get(Calendar.HOUR_OF_DAY);
+		int m = cal.get(Calendar.MINUTE);
+		int s = cal.get(Calendar.SECOND);
+
+		StringBuilder sb = new StringBuilder();
+
+		sb.append(_int2str(year, 4));
+		sb.append(_int2str(month, 2));
+		sb.append(_int2str(day, 2));
+		sb.append("_");
+		sb.append(_int2str(h, 2));
+		sb.append(_int2str(m, 2));
+		sb.append(_int2str(s, 2));
+
+		return sb.toString();
 	}
 
+	private static Object _int2str(int n, int width) {
+		String str = n + "";
+		if (n > 0) {
+			while (str.length() < width) {
+				str = "0" + str;
+			}
+		}
+		return str;
+	}
 }
